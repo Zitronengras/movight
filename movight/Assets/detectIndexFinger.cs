bb09dbd3cb56dd2943de02dc1a47bbeca955b067
Caro <carolin.stellmacher@haw-hamburg.de>
2016-01-25 16:44:41 +0100

    clean up

--- movight/Assets/detectIndexFinger.cs
+++ movight/Assets/detectIndexFinger.cs
@@ -12,108 +12,96 @@ namespace Leap{
 		Hand currentHand = new Hand();
 		Finger indexFinger = new Finger();
 		int indexFingerID = 1;
-		Bone indexBoneTip = new Bone ();
+		Bone distalBone = new Bone ();
 		float scan = 0.01f;
 		Vector3 normRay;
 
-		//WaitForEndOfFrame
+		Transform handController;
+		Vector3 handControllerPos;
 
-		//Transform lampTransform = lamp.transform;
+		//Test
+		Transform bulp;
+
 
 
 
 		// Use this for initialization
 		void Start () {
-			Transform lamp = GameObject.Find("lamp").transform; //.transform
-			//Controller controller = new Controller();
+
+			handController = GameObject.Find ("HeadMountedHandController").transform;
+			handControllerPos = handController.position;
+			//Debug.Log ("UNITY handControllerPos: " + handControllerPos);
+
 		}
 
 		// Update is called once per frame
 		void Update () {
-			//Debug.Log("update");
-
-			if (controller.IsConnected) {  //controller is a Controller object
+			
+			if (controller.IsConnected) {
 				frame = controller.Frame ();
-				//Debug.Log("controller found");
-
 			} else {
-				//Debug.Log("no controller found");
+				Debug.Log("no controller found");
 			}
 
-			HandList allHandsInFrame = frame.Hands; //gets Hands objects
+			//gets Hands objects
+			HandList allHandsInFrame = frame.Hands;
 
-			//Hand firstHand = hands.get (0);
-
-			//iterate hands
+			//iterate detected hands
 			if (!frame.Hands.IsEmpty) {
 				for(int i = 0; i < frame.Hands.Count; i++){
 
 					currentHand = allHandsInFrame[i];
+
 					//check for valid data
 					if (currentHand.IsValid) {
 						if (currentHand.IsRight) {
 							rightHand = currentHand;
-							//Debug.Log ("************************************************Found right hand" + rightHand);
+							//Debug.Log ("Found right hand" + rightHand);
+
+							//check for extended fingers on right hand
 							FingerList extendedFingers = rightHand.Fingers.Extended();
+
 							if (!extendedFingers.IsEmpty) {	
 								//Debug.Log("extended fingers found");
-								foreach (Finger currentExFinger in extendedFingers) {
-									//string fingerDescription = currentFinger.ToString();
-									//Debug.Log ("Finger description: " + fingerDescription);
+								foreach (Finger currentExtFinger in extendedFingers) {
+									
+									if (currentExtFinger.IsValid) {
+										
+										Finger.FingerType fingerType = currentExtFinger.Type;
 
-									if (currentExFinger.IsValid) {
-										Finger.FingerType fingerType = currentExFinger.Type;
 										if (fingerType == Finger.FingerType.TYPE_INDEX) {
-											string fingerDescription = currentExFinger.ToString();
-											//Debug.Log ("Finger description: " + fingerDescription);
-											indexFinger = currentExFinger;
-											Vector fingerTipPosition = indexFinger.TipPosition;
-											//Debug.Log ("LEAP FingerTipPosition: " + fingerTipPosition);
+											
+											indexFinger = currentExtFinger;
 											//Debug.Log ("found extended indexFinger");
-											//if (indexFinger.IsExtended) {
-											//Debug.Log ("extended indexFinger!");
-
-											//TODO: position entweder mit FingerTip oder center of distal
 
 											//find distal bone
-											indexBoneTip = indexFinger.Bone (Bone.BoneType.TYPE_DISTAL);
-											//foreach (Bone.BoneType boneType in (Bone.BoneType[]) Enum.GetValues(typeof(Bone.BoneType))) {
-											if (indexBoneTip.IsValid) {
+											distalBone = indexFinger.Bone (Bone.BoneType.TYPE_DISTAL);
+
+											if (distalBone.IsValid) {
 												//Debug.Log ("DISTAL Bone of indexFinger!");
 
-												Transform handController = GameObject.Find ("HeadMountedHandController").transform;
-												Vector3 handControllerPos = handController.position;
-												Debug.Log ("UNITY handControllerPos: " + handControllerPos);
-
-												/*Transform camera = GameObject.Find ("Main Camera").transform;
-												Vector3 cameraPos = camera.position;
-												Debug.Log ("UNITY cameraPos: " + cameraPos);*/
-
 												//get center point of distal
-												Vector middle = indexBoneTip.Center;
-												Debug.Log ("middle: " + middle);
+												Vector distalBoneCenter = distalBone.Center;
+												//Debug.Log ("distalBoneCenter: " + distalBoneCenter);
 
 												//transform into unityWorld
-												Vector3 unityMiddleScaled = middle.ToUnityScaled();
-												Debug.Log ("UNITY UnityMiddleScaled: " + unityMiddleScaled);
-
-
+												Vector3 unityDistalBoneCenter = distalBoneCenter.ToUnityScaled();
+												//Debug.Log ("UNITY unityDistalBoneCenter: " + unityDistalBoneCenter);
 
 												//rotate because of HMD
-												Vector3 rotUnityMiddleScaled = Quaternion.Euler(270, 180, 0) * unityMiddleScaled;
-												Debug.Log ("UNITY rotUnityMiddleScaled: " + rotUnityMiddleScaled);
+												Vector3 rotUnityDistalBoneCenter = Quaternion.Euler(270, 180, 0) * unityDistalBoneCenter;
+												//Debug.Log ("UNITY rotUnityMiddleScaled: " + rotUnityMiddleScaled);
 
 												//get head rotation
-												var rot = Cardboard.SDK.HeadRotation;
-												//GameObject head = GameObject.Find("Head").transform.position;
+												var headRotation = Cardboard.SDK.HeadRotation;
+												//rotate rotUnityDistalBoneCenter with headMovement
+												Vector3 distalControl = headRotation * rotUnityDistalBoneCenter;
 
-												//Quaternion headRot = rot.HeadRotation;
-												Vector3 final = rot * rotUnityMiddleScaled;
+												Debug.DrawRay (handControllerPos, distalControl*10, Color.cyan, 2.0f, true);
 
-												Debug.DrawRay (handControllerPos, final*10, Color.cyan, 2.0f, true);
-
-												Transform bulp = GameObject.Find("bulp").transform;
-												Vector3 bulpPos = bulp.position; //.transform.position;
+												//for test
+												bulp = GameObject.Find("bulp").transform;
+												Vector3 bulpPos = bulp.position;
 												Debug.Log ("bulp position: " + bulpPos);
 												//Raycast
 												//RaycastHit hit = new RaycastHit();
@@ -124,22 +112,6 @@ namespace Leap{
 													Debug.Log ("*********Lampe getroffen************");
 												}*/
 
-												//TODO: offset because of two raycast origins
-
-												//Debug.Log ("LEAP Middle of DISTAL: " + middle);
-												//leap to unity world
-												//Vector3 unityMiddle = middle.ToUnity();
-												//Debug.Log ("UNITY Middle.ToUnity() of DISTAL: " + unityMiddle);
-
-												/*Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL: " + unityMiddleScaled);
-												Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL X: " + unityMiddleScaled.x);
-												Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL Y: " + unityMiddleScaled.y);
-												Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL Z: " + unityMiddleScaled.z);*/
-
-
-												/*Debug.Log ("UNITY rotVector X: " + rotUnityMiddleScaled.x);
-												Debug.Log ("UNITY rotVector (old Z) new Y: " + rotUnityMiddleScaled.y);
-												Debug.Log ("UNITY rotVector (old Y) new Z: " + rotUnityMiddleScaled.z);*/
 											}
 										}
 									}
