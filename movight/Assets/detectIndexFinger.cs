fe8626152f58657d276c49324c15c2d1563dd373
Caro <carolin.stellmacher@haw-hamburg.de>
2016-01-25 01:17:18 +0100

    raycaster works from controller to indexFinger

--- movight/Assets/detectIndexFinger.cs
+++ movight/Assets/detectIndexFinger.cs
@@ -46,7 +46,6 @@ namespace Leap{
 			if (!frame.Hands.IsEmpty) {
 				for(int i = 0; i < frame.Hands.Count; i++){
 					
-					//Debug.Log("all hands" + allHandsInFrame[i]); //System.Diagnostics.Debug.Write
 					currentHand = allHandsInFrame[i];
 					//check for valid data
 					if (currentHand.IsValid) {
@@ -71,23 +70,51 @@ namespace Leap{
 											//Debug.Log ("found extended indexFinger");
 											//if (indexFinger.IsExtended) {
 												//Debug.Log ("extended indexFinger!");
+
+											//TODO: position entweder mit FingerTip oder center of distal
+
 											//find distal bone
 											indexBoneTip = indexFinger.Bone (Bone.BoneType.TYPE_DISTAL);
 											//foreach (Bone.BoneType boneType in (Bone.BoneType[]) Enum.GetValues(typeof(Bone.BoneType))) {
 											if (indexBoneTip.IsValid) {
 												//Debug.Log ("DISTAL Bone of indexFinger!");
 
-												Vector middle = indexBoneTip.Center;
-												//Debug.Log ("LEAP Middle of DISTAL: " + middle);
-												//leap to unity world
-												Vector3 unityMiddle = middle.ToUnityScaled();
-												Debug.Log ("UNITY Middle of DISTAL: " + unityMiddle);
-
 												Transform handController = GameObject.Find ("HeadMountedHandController").transform;
 												Vector3 handControllerPos = handController.position;
+												Debug.Log ("UNITY handControllerPos: " + handControllerPos);
 
-												Debug.DrawRay (handControllerPos, unityMiddle, Color.red, 80.0f, false); 
+												/*Transform camera = GameObject.Find ("Main Camera").transform;
+												Vector3 cameraPos = camera.position;
+												Debug.Log ("UNITY cameraPos: " + cameraPos);*/
 
+												//get center point of distal
+												Vector middle = indexBoneTip.Center;
+												//transform into unityWorld
+												Vector3 unityMiddleScaled = middle.ToUnityScaled();
+												//rotate because of HMD
+												Vector3 rotUnityMiddleScaled = Quaternion.Euler(270, 180, 0) * unityMiddleScaled;
+												//rotate because of two eyes (get the middle)
+												//rotUnityMiddleScaled.x =  (rotUnityMiddleScaled.x + 0.5f);
+
+												//TODO: offset because of two raycast origins
+											
+												//Debug.Log ("LEAP Middle of DISTAL: " + middle);
+												//leap to unity world
+												//Vector3 unityMiddle = middle.ToUnity();
+												//Debug.Log ("UNITY Middle.ToUnity() of DISTAL: " + unityMiddle);
+
+												/*Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL: " + unityMiddleScaled);
+												Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL X: " + unityMiddleScaled.x);
+												Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL Y: " + unityMiddleScaled.y);
+												Debug.Log ("UNITY Middle.ToUnityScaled() of DISTAL Z: " + unityMiddleScaled.z);*/
+
+												Debug.Log ("UNITY rotUnityMiddleScaled: " + rotUnityMiddleScaled);
+												/*Debug.Log ("UNITY rotVector X: " + rotUnityMiddleScaled.x);
+												Debug.Log ("UNITY rotVector (old Z) new Y: " + rotUnityMiddleScaled.y);
+												Debug.Log ("UNITY rotVector (old Y) new Z: " + rotUnityMiddleScaled.z);*/
+
+
+												Debug.DrawRay (handControllerPos, rotUnityMiddleScaled*10, Color.red, 20.0f, true);
 
 												Transform lamp = GameObject.Find("lamp").transform;
 												Vector3 lampPos = lamp.position; //.transform.position;
@@ -95,7 +122,7 @@ namespace Leap{
 
 
 
-												//TODO: position entweder mit FingerTip oder center of distal
+											
 											}
 												//}
 
