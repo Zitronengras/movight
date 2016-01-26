c82c7b3fd9f3039a5a197f80d4ff411d4ed50c95
Caro <carolin.stellmacher@haw-hamburg.de>
2016-01-26 15:08:47 +0100

    add empty script for countdown when selecting light

---
+++ movight/Assets/selectCountdown.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectCountdown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("SELECT COUNTDOWN");
+	}
+}
