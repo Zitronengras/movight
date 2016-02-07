2bec063df4925ebc7aadbce76f10eb8592a287d7
Caro <carolin.stellmacher@haw-hamburg.de>
2016-02-07 19:51:20 +0100

    add main menu to change between versions

---
+++ movight/Assets/ownScripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button groupA;
+	public Button groupB;
+
+	// Use this for initialization
+	void Start () {
+		
+		groupA = groupA.GetComponent<Button>();
+		groupB = groupB.GetComponent<Button>();
+	
+	}
+
+	public void LoadSceneA(){
+		SceneManager.LoadScene("InteractionGroupA");
+	}
+	public void LoadSceneB(){
+		SceneManager.LoadScene("InteractionGroupB");
+	}
+
+}
